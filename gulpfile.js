ec5f78dd2a5fe13e596ab7ee73bff7408939fb1a
William Grasel <william.grasel@scup.com>
2015-08-03 13:50:53 +0530

    building component according to modularization strategy

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('build', function (callback) {
-	return gulp.src('./index.js')
-    	.pipe(babel())
-    	.pipe(gulp.dest('./dist'));
+    return gulp.src('./index.jsx')
+        .pipe(babel({ blacklist: ["strict"] }))
+        .pipe(gulp.dest('./dist'));
 });
