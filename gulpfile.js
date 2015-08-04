38cb0dbe45232ce51f5dfea55da492d152b93942
Cameron Nokes <ccnokes@gmail.com>
2015-08-03 23:05:39 -0600

    minor code style changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('build', function (callback) {
+gulp.task('build', function () {
     return gulp.src('./index.jsx')
-        .pipe(babel({ blacklist: ["strict"] }))
+        .pipe(babel({ blacklist: ['strict'] }))
         .pipe(gulp.dest('./dist'));
 });
